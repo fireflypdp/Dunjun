a935afe3f5909bee265e613ba9ce45bb829bcf75
Peter Pascoal <peter.pascoal@gmail.com>
2015-12-01 22:00:43 -0800

    Day 01

---
+++ include/Dunjun/Common.h
@@ -0,0 +1,12 @@
+#ifndef DUNJUN_COMMON_H
+#define DUNJUN_COMMON_H
+
+namespace Dunjun
+{
+	typedef float f32;
+	typedef double f64;
+	typedef int s32;
+	typedef unsigned int u32;
+} // namespace Dunjun
+
+#endif//DUNJUN_COMMON_H
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <Dunjun/Common.h>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello\n";
+	std::cin.ignore();
+
+	return 0;
+}
